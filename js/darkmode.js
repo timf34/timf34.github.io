fddfa10dba7078b9d3af1af55dc9e041d4329368
timf34 <farrelti@tcd.ie>
2023-01-16 00:23:01 +0000

    temp fix so logo shows when

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,13 +2,23 @@
 const currentMode = localStorage.getItem('mode');
 if (currentMode) {
     document.documentElement.classList.add(currentMode);
-    if (currentMode === "dark-mode") {
-        toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
-    }
+    // if (currentMode === "dark-mode") {
+    //     console.log("dark mode");
+    //     // toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
+    // }
 }
 
 window.onload = function() {
     const toggleMode = document.getElementById('toggle-mode');
+
+    // Messy fix where toggleMode is null when using the commented out code above, as the button hasn't loaded yet. This is a temporary fix.
+    if (currentMode) {
+        document.documentElement.classList.add(currentMode);
+        if (currentMode === "dark-mode") {
+            console.log("dark mode");
+            toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
+        }
+    }
 
     toggleMode.addEventListener('click', () => {
         console.log("clicked!")
