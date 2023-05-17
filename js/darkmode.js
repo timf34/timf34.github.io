1394a1770fb7c24c73b52b26e37e0e56bbebce83
timf34 <farrelti@tcd.ie>
2023-05-17 18:43:17 +0100

    Refactored dark mode: changed from window.onload to a DOMContentLoaded listener which allows us to run two scripts on our faves page and is better practice

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,31 +1,25 @@
-// Keeping this block of code outside of window.onload to ensure that the theme is set on page load (no white flashes!)
+// Keeping this block of code outside of event listeners to ensure that the theme is set on page load (no white flashes!)
 const currentMode = localStorage.getItem('mode');
 if (currentMode) {
     document.documentElement.classList.add(currentMode);
-    // if (currentMode === "dark-mode") {
-    //     console.log("dark mode");
-    //     // toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
-    // }
 }
 
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function() {
     const toggleMode = document.getElementById('toggle-mode');
 
-    // Messy fix where toggleMode is null when using the commented out code above, as the button hasn't loaded yet. This is a temporary fix.
+    // Update toggle mode button if currentMode is set
     if (currentMode) {
-        document.documentElement.classList.add(currentMode);
         if (currentMode === "dark-mode") {
-            console.log("dark mode");
             toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
         }
     }
 
     toggleMode.addEventListener('click', () => {
-        console.log("clicked!")
+        console.log("clicked!");
 
         const currentMode = localStorage.getItem('mode');
 
-        // toggle mode
+        // Toggle mode
         if (currentMode === 'dark-mode') {
             document.documentElement.classList.remove('dark-mode');
             document.documentElement.classList.add('light-mode');
@@ -43,4 +37,4 @@ window.onload = function() {
             toggleMode.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" aria-label="Moon Icon" fill="currentColor" stroke="none" width="30" height="30" class="inline-block" viewBox="0 0 20 20" id="dark-mode-icon"> <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path> </svg>';
         }
     });
-}
+});
