b1a5402ace7f57abb4ec5f68749d29cf19752f67
timf34 <farrelti@tcd.ie>
2023-05-17 18:35:00 +0100

    added the year to the data structure, and adjusted the code to work with it

--- faves/faves.js
+++ faves/faves.js
@@ -19,7 +19,7 @@ window.onload = function() {
 
     let data = {
         "music": {
-            "May": [
+            "May 2023": [
                 { name: "Sway by Michael Bublé", url: "https://open.spotify.com/track/2ajUl8lBLAXOXNpG4NEPMz?si=84b1f38d9c9545be" },
                 { name: "Scheherezade by Rimsky-Korsakov", url: "https://open.spotify.com/album/3QXtNXmOyrOfZ2mWG4rw9v?si=OCTWg4FITpqYOzkKWCBZTA" },
                 { name: "jazz is for ordinary people by Berlioz", url: "https://open.spotify.com/track/12BaQt9aYdTlEtKreqB5V4?si=a0e7e15ef13640b0" }
@@ -27,7 +27,15 @@ window.onload = function() {
             // Continue for each month
         },
         "movies": {
-            "Spring": [
+            "Winter 2022": [
+                { name: "From up on Poppy Hill", url: "" },
+                { name: "The Mitchells vs the Machines", url: "" },
+                { name: "Avatar 2", url: "" },
+                { name: "Children of Men", url: "" },
+                { name: "City of God", url: "" },
+                { name: "Triangle of Sadness", url: "" }
+            ],
+            "Spring 2023": [
                 { name: "Puss in Boots: The Last Wish", url: "" },
                 { name: "Vicky Cristina Barcelona", url: "" },
                 { name: "Knock at the Cabin", url: "" }
@@ -37,11 +45,11 @@ window.onload = function() {
     };
 
 
-    function updateList(category, month) {
+    function updateList(category, periodYear) {
         let list = document.getElementById(category + "List");
         list.innerHTML = "";
 
-        for (let item of data[category][month]) {
+        for (let item of data[category][periodYear]) {
             let listItem = document.createElement("li");
 
             // Create an anchor element
@@ -56,30 +64,27 @@ window.onload = function() {
             list.appendChild(listItem);
         }
 
-        document.getElementById(category + "Header").textContent = `${capitalizeFirstLetter(category)} - ${month}`;
+        document.getElementById(category + "Header").textContent = `${capitalizeFirstLetter(category)} - ${periodYear}`;
     }
 
 
     function headerClick(event) {
-
         console.log("Header clicked: " + event.target.id);
 
         let category = event.target.id.replace("Header", "");
-        console.log("Category:", category); // Add this line
 
         // Create the menu
         let menu = document.createElement("div");
         menu.id = "menu";
 
-        // Period can be month or season
-        for (let period in data[category]) {
-            let periodOption = document.createElement("button");
-            periodOption.textContent = period;
-            periodOption.addEventListener("click", function () {
-                updateList(category, period);
+        for (let periodYear in data[category]) {
+            let periodYearOption = document.createElement("button");
+            periodYearOption.textContent = periodYear;
+            periodYearOption.addEventListener("click", function () {
+                updateList(category, periodYear);
                 menu.parentNode.removeChild(menu); // remove menu after selection
             });
-            menu.appendChild(periodOption);
+            menu.appendChild(periodYearOption);
         }
 
         // Remove any existing menu
@@ -89,49 +94,49 @@ window.onload = function() {
         }
 
         // Add the new menu to the document
-        // event.target.appendChild(menu);
         event.target.insertAdjacentElement('afterend', menu);
-
     }
+
 
 
     // Get the current month name
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    // Get the current month index
+// Get the current month index and year
     let currentMonthIndex = new Date().getMonth();
+    let currentYear = new Date().getFullYear();
 
-    // Check if data for the current month exists, if not, find the most recent month that does exist
+// Check if data for the current month and year exists, if not, find the most recent month that does exist
     let monthToDisplay = null;
     for (let i = currentMonthIndex; i >= 0; i--) {
         let monthName = monthNames[i];
-        if (data["music"][monthName]) { // Replace "music" with the category you're checking
-            monthToDisplay = monthName;
+        if (data["music"][`${monthName} ${currentYear}`]) {
+            monthToDisplay = `${monthName} ${currentYear}`;
             break;
         }
     }
 
-    // If no data was found for any month this year, default to December of the previous year
+// If no data was found for any month this year, default to December of the previous year
     if (!monthToDisplay) {
-        monthToDisplay = "December";
+        monthToDisplay = `December ${currentYear - 1}`;
     }
 
-    // Set the default date for each category when the page loads
+// Set the default date for each category when the page loads
     updateList("music", monthToDisplay);
 
-    // Get the current season
+// Get the current season
     let currentSeason = getSeason(new Date());
 
-    // Check if data for the current season exists
-    if (data["movies"][currentSeason]) {
-        updateList("movies", currentSeason);
+// Check if data for the current season and year exists
+    if (data["movies"][`${currentSeason} ${currentYear}`]) {
+        updateList("movies", `${currentSeason} ${currentYear}`);
     } else {
-        // If no data was found for the current season, you could default to a specific season, or find the most recent season with data, similar to how you did for the months.
-        updateList["movies"]["Spring"];
+        updateList("movies", `Spring 2023`);
     }
 
 
-    document.getElementById("musicHeader").addEventListener("click", headerClick);
+
+        document.getElementById("musicHeader").addEventListener("click", headerClick);
     document.getElementById("moviesHeader").addEventListener("click", headerClick);
 
     console.log("Loaded faves.js")
