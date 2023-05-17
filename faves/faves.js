49dbee585769e8b647710c9b6725521b72ad4aab
timf34 <farrelti@tcd.ie>
2023-05-17 20:31:54 +0100

    Refactoring; decomposed the updateList func; changed data from variable to function

--- faves/faves.js
+++ faves/faves.js
@@ -29,8 +29,141 @@ window.onload = function () {
 
     let menu;
     let outsideClickListener;
+    let data = initializeData();
 
-    let data = {
+    // Initial setup
+    let currentMonthIndex = new Date().getMonth();
+    let currentYear = new Date().getFullYear();
+    let currentSeason = getSeasonFromDate(new Date());
+    setInitialDisplayData(data, currentMonthIndex, currentYear, currentSeason);
+
+    // Initial Display
+    function setInitialDisplayData(data, currentMonthIndex, currentYear, currentSeason) {
+        let monthToDisplay = getMostRecentMonth(data, currentMonthIndex, currentYear);
+        updateList("music", monthToDisplay, data);
+        let seasonToDisplay = data["movies"][`${currentSeason} ${currentYear}`] ? `${currentSeason} ${currentYear}` : `Spring 2023`;
+        updateList("movies", seasonToDisplay, data);
+    }
+
+    function closeMenuIfClickedOutside(e) {
+        if (menu && !menu.contains(e.target) && e.target.id.indexOf("Header") === -1) { // ignore header clicks
+            if (menu.parentNode) {
+                menu.parentNode.removeChild(menu);
+                document.removeEventListener('click', closeMenuIfClickedOutside);
+            } else {
+                console.log("menu.parentNode is null");
+            }
+        }
+    }
+
+    function headerClick(event) {
+        console.log("Header clicked: " + event.target.id);
+
+        let category = event.target.id.replace("Header", "");
+
+        // Create the menu
+        menu = document.createElement("div");
+        menu.id = "menu";
+
+        for (let periodYear in data[category]) {
+            let periodYearOption = document.createElement("button");
+            periodYearOption.textContent = periodYear;
+            periodYearOption.addEventListener("click", function () {
+                updateList(category, periodYear, data);
+                setTimeout(() => {
+                    if (menu.parentNode) {
+                        menu.parentNode.removeChild(menu); // remove menu after selection
+                    }
+                }, 0);
+            });
+            menu.appendChild(periodYearOption);
+        }
+
+        // Remove any existing menu
+        let oldMenu = document.getElementById("menu");
+        if (oldMenu) {
+            oldMenu.parentNode.removeChild(oldMenu);
+        }
+
+        // Add the new menu to the document
+        event.target.insertAdjacentElement('afterend', menu);
+
+        // Remove old listener before adding a new one
+        if (outsideClickListener) {
+            document.removeEventListener('click', outsideClickListener);
+        }
+
+        // Reference for the function so we can remove it later
+        outsideClickListener = function (e) {
+            closeMenuIfClickedOutside(e, menu);
+        };
+
+        document.addEventListener('click', outsideClickListener);
+    }
+
+    // Get the current month name
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Check if data for the current month and year exists, if not, find the most recent month that does exist
+    let monthToDisplay = null;
+    for (let i = currentMonthIndex; i >= 0; i--) {
+        let monthName = monthNames[i];
+        if (data["music"][`${monthName} ${currentYear}`]) {
+            monthToDisplay = `${monthName} ${currentYear}`;
+            break;
+        }
+    }
+
+// If no data was found for any month this year, default to December of the previous year
+    if (!monthToDisplay) {
+        monthToDisplay = `December ${currentYear - 1}`;
+    }
+
+// Set the default date for each category when the page loads
+    updateList("music", monthToDisplay, data);
+
+
+// Check if data for the current season and year exists
+    if (data["movies"][`${currentSeason} ${currentYear}`]) {
+        updateList("movies", `${currentSeason} ${currentYear}`, data);
+    } else {
+        updateList("movies", `Spring 2023`, data);
+    }
+
+    document.getElementById("musicHeader").addEventListener("click", headerClick);
+    document.getElementById("moviesHeader").addEventListener("click", headerClick);
+}
+
+function updateList(category, periodYear, data) {
+    let list = document.getElementById(category + "List");
+    list.innerHTML = "";
+
+    for (let item of data[category][periodYear]) {
+        let listItem = createListItem(item);
+        list.appendChild(listItem);
+    }
+
+    document.getElementById(category + "Header").textContent = `${capitalizeFirstLetter(category)} - ${periodYear}`;
+}
+
+function createListItem(item) {
+    let listItem = document.createElement("li");
+
+    // Create an anchor element
+    let link = document.createElement("a");
+    link.href = item.url;
+    link.textContent = item.name;
+    link.target = "_blank"; // Optional: open the link in a new tab
+
+    // Append the anchor element to the list item
+    listItem.appendChild(link);
+
+    return listItem;
+}
+
+function initializeData() {
+    // TODO: make this into a JSON file that I read!
+    return {
         "music": {
             "May 2023": [
                 {
@@ -63,133 +196,4 @@ window.onload = function () {
             ],
         },
     };
-
-    // Initial setup
-    let currentMonthIndex = new Date().getMonth();
-    let currentYear = new Date().getFullYear();
-    let currentSeason = getSeasonFromDate(new Date());
-    setInitialDisplayData(data, currentMonthIndex, currentYear, currentSeason);
-
-    function updateList(category, periodYear) {
-        let list = document.getElementById(category + "List");
-        list.innerHTML = "";
-
-        console.log(periodYear);
-
-        for (let item of data[category][periodYear]) {
-            let listItem = document.createElement("li");
-
-            // Create an anchor element
-            let link = document.createElement("a");
-            link.href = item.url;
-            link.textContent = item.name;
-            link.target = "_blank"; // Optional: open the link in a new tab
-
-            // Append the anchor element to the list item
-            listItem.appendChild(link);
-
-            list.appendChild(listItem);
-        }
-
-        document.getElementById(category + "Header").textContent = `${capitalizeFirstLetter(category)} - ${periodYear}`;
-    }
-
-    // Initial Display
-    function setInitialDisplayData(data, currentMonthIndex, currentYear, currentSeason) {
-        let monthToDisplay = getMostRecentMonth(data, currentMonthIndex, currentYear);
-        updateList("music", monthToDisplay);
-        let seasonToDisplay = data["movies"][`${currentSeason} ${currentYear}`] ? `${currentSeason} ${currentYear}` : `Spring 2023`;
-        updateList("movies", seasonToDisplay);
-    }
-
-    function closeMenuIfClickedOutside(e) {
-        if (menu && !menu.contains(e.target) && e.target.id.indexOf("Header") === -1) { // ignore header clicks
-            if (menu.parentNode) {
-                menu.parentNode.removeChild(menu);
-                document.removeEventListener('click', closeMenuIfClickedOutside);
-            } else {
-                console.log("menu.parentNode is null");
-            }
-        }
-    }
-
-    function headerClick(event) {
-        console.log("Header clicked: " + event.target.id);
-
-        let category = event.target.id.replace("Header", "");
-
-        // Create the menu
-        menu = document.createElement("div");
-        menu.id = "menu";
-
-        for (let periodYear in data[category]) {
-            let periodYearOption = document.createElement("button");
-            periodYearOption.textContent = periodYear;
-            periodYearOption.addEventListener("click", function () {
-                updateList(category, periodYear);
-                setTimeout(() => {
-                    if (menu.parentNode) {
-                        menu.parentNode.removeChild(menu); // remove menu after selection
-                    }
-                }, 0);
-            });
-            menu.appendChild(periodYearOption);
-        }
-
-        // Remove any existing menu
-        let oldMenu = document.getElementById("menu");
-        if (oldMenu) {
-            oldMenu.parentNode.removeChild(oldMenu);
-        }
-
-        // Add the new menu to the document
-        event.target.insertAdjacentElement('afterend', menu);
-
-        // Remove old listener before adding a new one
-        if (outsideClickListener) {
-            document.removeEventListener('click', outsideClickListener);
-        }
-
-        // Reference for the function so we can remove it later
-        outsideClickListener = function (e) {
-            closeMenuIfClickedOutside(e, menu);
-        };
-
-        document.addEventListener('click', outsideClickListener);
-    }
-
-
-    // Get the current month name
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-// Check if data for the current month and year exists, if not, find the most recent month that does exist
-    let monthToDisplay = null;
-    for (let i = currentMonthIndex; i >= 0; i--) {
-        let monthName = monthNames[i];
-        if (data["music"][`${monthName} ${currentYear}`]) {
-            monthToDisplay = `${monthName} ${currentYear}`;
-            break;
-        }
-    }
-
-// If no data was found for any month this year, default to December of the previous year
-    if (!monthToDisplay) {
-        monthToDisplay = `December ${currentYear - 1}`;
-    }
-
-// Set the default date for each category when the page loads
-    updateList("music", monthToDisplay);
-
-
-// Check if data for the current season and year exists
-    if (data["movies"][`${currentSeason} ${currentYear}`]) {
-        updateList("movies", `${currentSeason} ${currentYear}`);
-    } else {
-        updateList("movies", `Spring 2023`);
-    }
-
-    document.getElementById("musicHeader").addEventListener("click", headerClick);
-    document.getElementById("moviesHeader").addEventListener("click", headerClick);
 }
-
-
