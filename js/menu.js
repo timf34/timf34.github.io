7a9c6305e4c22005218ae60ba3334c3d290f4e67
timf34 <farrelti@tcd.ie>
2023-06-19 21:17:26 +0100

    menu burger button now working... added check to ensure that DOM is loaded to the JS file

---
+++ js/menu.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('navbarToggle').addEventListener('click', function() {
+        document.querySelector('.navbar-links').classList.toggle('show');
+    });
+});
