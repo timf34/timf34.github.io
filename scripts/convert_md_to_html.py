6a5aa238f33768d2607ec4b133c61a9e94434bf0
timf34 <farrelti@tcd.ie>
2023-09-05 15:10:23 +0100

    refactored script

--- scripts/convert_md_to_html.py
+++ scripts/convert_md_to_html.py
@@ -1,21 +1,35 @@
-# Script for converting .md files to .html files in the lists directory
-
 import os
 import subprocess
 
-# Path to the directory with the .md files
-directory = '../lists'  # Lists directory
+DIRECTORY = '../lists'  # Path to the directory with .md files
+TEMPLATE_PATH = os.path.join(DIRECTORY, 'template.html')  # Full path to the HTML template
 
-# Full path to the template.html file
-template_path = os.path.join(directory, 'template.html')
+def convert_md_to_html(directory: str, template_path: str) -> None:
+    """
+    Convert .md files in a directory to .html using pandoc with a specified template.
 
-# Loop through each file in the directory
-for filename in os.listdir(directory):
-    if filename.endswith(".md"):
-        full_md_path = os.path.join(directory, filename)
-        output_name = filename.rsplit('.', 1)[0] + '.html'  # Replace .md with .html
-        output_path = os.path.join(directory, output_name)
-        # Run the pandoc command to convert .md to .html
-        subprocess.run(['pandoc', '-s', full_md_path, f'--template={template_path}', '-o', output_path])
+    Args:
+        directory (str): Path to the directory containing the .md files.
+        template_path (str): Full path to the HTML template to be used by pandoc.
+    """
 
-print("Conversion complete!")
+    # Iterate over all files in the specified directory
+    for filename in os.listdir(directory):
+
+        # Check if the file is a Markdown file
+        if filename.endswith(".md"):
+            # Combine the directory path with the filename to get the full path for the .md file
+            full_md_path = os.path.join(directory, filename)
+
+            # Replace the .md extension with .html for the output file
+            output_path = os.path.join(directory, filename.rsplit('.', 1)[0] + '.html')
+
+            # Run the pandoc command to convert the markdown file to HTML using the given template
+            subprocess.run(['pandoc', '-s', full_md_path, f'--template={template_path}', '-o', output_path])
+
+def main() -> None:
+    convert_md_to_html(DIRECTORY, TEMPLATE_PATH)
+    print("Conversion complete!")
+
+if __name__ == "__main__":
+    main()
